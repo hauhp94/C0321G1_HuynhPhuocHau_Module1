e64d7101680ae3704af6aeb488a057ceb37475b0
Hau HP <hauhpdn2015@gmail.com>
2021-04-02 15:00:31 +0700

    bt

---
+++ 7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh_7/thuchanhtoantu.js
@@ -0,0 +1,6 @@
+inputWidth =prompt("enter the width");
+inputHeight =prompt("enter the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width*height;
+document.write("diện tích là "+ area);
